6444a75dac42e66a1be595b303731e80ed17dd36
elameen <elameendaiyabu@gmail.com>
2024-11-04 12:52:01 +0000

    proj 2: implemented func to get cpu info

--- beginner/system-info-script/main.go
+++ beginner/system-info-script/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/host"
 	"github.com/shirou/gopsutil/v4/mem"
 )
@@ -23,8 +24,19 @@ type HostInfo struct {
 	HostName, Os, Platform struct{ name, val string }
 }
 
+type CpuInfo struct {
+	ModelName struct {
+		name string
+		val  string
+	}
+	CacheSize, ProcessorSpeed, NumberOfCores struct {
+		name string
+		val  int
+	}
+}
+
 func MemoryInfo(m mem.VirtualMemoryStat) MemInfo {
-	var memInfo MemInfo = MemInfo{
+	memInfo := MemInfo{
 		Total: struct {
 			name string
 			val  uint64
@@ -51,7 +63,7 @@ func MemoryInfo(m mem.VirtualMemoryStat) MemInfo {
 }
 
 func HostInformation(h host.InfoStat) HostInfo {
-	var hostInfo HostInfo = HostInfo{
+	hostInfo := HostInfo{
 		HostName: struct {
 			name string
 			val  string
@@ -78,6 +90,40 @@ func HostInformation(h host.InfoStat) HostInfo {
 	return hostInfo
 }
 
+func CpuInformation(c cpu.InfoStat, numOfCores int) CpuInfo {
+	cpuinfo := CpuInfo{
+		ModelName: struct {
+			name string
+			val  string
+		}{
+			name: "Model Name",
+			val:  c.ModelName,
+		},
+		CacheSize: struct {
+			name string
+			val  int
+		}{
+			name: "Cache Size",
+			val:  int(c.CacheSize),
+		},
+		ProcessorSpeed: struct {
+			name string
+			val  int
+		}{
+			name: "Processor Speed",
+			val:  int(c.Mhz),
+		},
+		NumberOfCores: struct {
+			name string
+			val  int
+		}{
+			name: "Number of CPU Cores",
+			val:  numOfCores,
+		},
+	}
+	return cpuinfo
+}
+
 func main() {
 	// get memory info
 	m, err := mem.VirtualMemory()
@@ -99,5 +145,16 @@ func main() {
 	hostInfo := HostInformation(*h)
 	fmt.Printf("%s: %s \n", hostInfo.HostName.name, hostInfo.HostName.val)
 	fmt.Printf("%s: %s \n", hostInfo.Os.name, hostInfo.Os.val)
-	fmt.Printf("%s: %s \n", hostInfo.Platform.name, hostInfo.Platform.val)
+	fmt.Printf("%s: %s \n\n", hostInfo.Platform.name, hostInfo.Platform.val)
+
+	// get cpu info
+	c, err := cpu.Info()
+	if err != nil {
+		log.Println(err)
+	}
+	cpuInfo := CpuInformation(c[0], len(c))
+	fmt.Printf("%s: %s \n", cpuInfo.ModelName.name, cpuInfo.ModelName.val)
+	fmt.Printf("%s: %d \n", cpuInfo.CacheSize.name, cpuInfo.CacheSize.val)
+	fmt.Printf("%s: %d \n", cpuInfo.ProcessorSpeed.name, cpuInfo.ProcessorSpeed.val)
+	fmt.Printf("%s: %d \n", cpuInfo.NumberOfCores.name, cpuInfo.NumberOfCores.val)
 }
