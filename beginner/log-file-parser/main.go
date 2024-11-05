e3ff6510efa895c419106b2d0ed4c29fb288a886
elameen <elameendaiyabu@gmail.com>
2024-11-05 20:32:34 +0000

    proj 3: finished journatctl logs parser

--- beginner/log-file-parser/main.go
+++ beginner/log-file-parser/main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"regexp"
+	"strings"
 )
 
-func main() {
-	logExample := `Nov 05 08:47:35 bloodhound kernel: [drm:drm_new_set_master] *ERROR* [nvidia-drm] [GPU ID 0x00000100] Failed to grab modeset ownership`
-	logFormat := `(?P<month>\w+) (?P<day>\d{2}) (?P<time>\d{2}:\d{2}:\d{2}) (?P<hostname>\w+) (?P<service>\w+): \[(?P<component>.*?)\] \*(?P<log_level>.*?)\* \[(?P<driver>.*?)\] \[(?P<gpu_id>.*?)\] (?P<message>.*)`
+func GetFileLines() []string {
+	f, _ := os.Open("./log.txt")
+	defer f.Close()
 
-	re := regexp.MustCompile(logFormat)
-	matches := re.FindStringSubmatch(logExample)
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	var fileLines []string
 
-	for i, k := range re.SubexpNames() {
-		// ignore the first and the $_
-		if i == 0 || k == "_" {
-			continue
-		}
-
-		fmt.Printf("%-15s => %s\n", k, matches[i])
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
 	}
+
+	return fileLines
+}
+
+func ParseLogFileLines(re *regexp.Regexp, fileLines []string) {
+	for idx, line := range fileLines {
+		matches := re.FindStringSubmatch(line)
+		if matches == nil {
+			return
+		}
+		fmt.Printf("%s %d\n", strings.ToUpper("error"), idx+1)
+		for i, k := range re.SubexpNames() {
+			// ignore the first and the $_
+			if i == 0 || k == "_" {
+				continue
+			}
+
+			fmt.Printf("%-15s => %s\n", k, matches[i])
+		}
+		fmt.Printf("\n\n")
+	}
+}
+
+func main() {
+	logFormat := `(?P<month>\w+) (?P<day>\d{2}) (?P<time>\d{2}:\d{2}:\d{2}) (?P<hostname>\S+) (?P<service>\S+):(?: \[(?P<component>[^\]]*)\])?(?: \*(?P<log_level>[^\*]+)\*)?(?: \[(?P<driver>[^\]]+)\])?(?: \[(?P<gpu_id>[^\]]+)\])? (?P<message>.+)`
+	re := regexp.MustCompile(logFormat)
+
+	fileLines := GetFileLines()
+
+	ParseLogFileLines(re, fileLines)
 }
