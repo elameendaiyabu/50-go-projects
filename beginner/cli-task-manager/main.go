b8009e266e24784e27ed0df950b8db754c3ed62a
elameen <elameendaiyabu@gmail.com>
2024-11-07 21:28:35 +0000

    proj 5: update and delete func done

--- beginner/cli-task-manager/main.go
+++ beginner/cli-task-manager/main.go
@@ -2,26 +2,28 @@ package main
 
 import "fmt"
 
-type Task map[int]string
+type Tasks map[int]string
 
-func PrintAllTasks(tasks map[int]string) {
-	for id, task := range tasks {
-		fmt.Printf("%d. %s", task[id], task)
-	}
-}
-
-func (t Task) Add(task string) {
-	t[len(t)+1] = task
-}
-
-func (t Task) GetTasks() {
+func (t Tasks) GetTasks() {
 	for idx, task := range t {
 		fmt.Printf("%d. %s\n", idx, task)
 	}
 }
 
+func (t Tasks) Add(task string) {
+	t[len(t)+1] = task
+}
+
+func (t Tasks) Update(id int, newTask string) {
+	t[id] = newTask
+}
+
+func (t Tasks) Delete(id int) {
+	delete(t, id)
+}
+
 func main() {
-	var tasks Task
+	var tasks Tasks
 	tasks = make(map[int]string)
 	tasks.Add("watch")
 	tasks.Add("clean")
