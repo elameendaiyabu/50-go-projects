fdacd1ca17d2099eef9ab319cbf1f67327be84b6
Elameen Daiyabu <elameendaiyabu@proton.me>
2024-11-10 19:13:49 +0000

    proj 8: can read yaml file that stores urls

---
+++ beginner/http-status-checker/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type url struct {
+	Url string
+}
+
+type endpoints struct {
+	Urls []url
+}
+
+var DestinationUrl *endpoints
+
+// getting urls stored in a yaml file
+func getURLs() *endpoints {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("urls")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(`.`, `_`))
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal(err)
+	}
+	if err := viper.Unmarshal(&DestinationUrl); err != nil {
+		log.Fatal(err)
+	}
+
+	return DestinationUrl
+}
+
+func main() {
+	// getting the urls
+	dest := getURLs()
+	fmt.Println(dest)
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Status)
+}
