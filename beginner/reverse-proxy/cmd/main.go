c233c7c0855c17784a829eea3ef40e3d4c88d29b
Elameen Daiyabu <elameendaiyabu@proton.me>
2024-11-10 14:59:07 +0000

    proj 7: wrote server proxy

--- beginner/reverse-proxy/cmd/main.go
+++ beginner/reverse-proxy/cmd/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"go-projects/beginner/reverse-proxy/internal/server"
+	"log"
+)
+
+func main() {
+	err := server.Run()
+	if err != nil {
+		log.Fatalf("could not start server: %v", err)
+	}
+}
