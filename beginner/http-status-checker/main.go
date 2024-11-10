ace79eb23a1bed06827606aeca48d74fb273c340
Elameen Daiyabu <elameendaiyabu@proton.me>
2024-11-10 21:54:06 +0000

    proj 8: wrote main func to check url status

--- beginner/http-status-checker/main.go
+++ beginner/http-status-checker/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/spf13/viper"
 )
@@ -16,10 +17,16 @@ type endpoints struct {
 	Urls []url
 }
 
+// type for channel to grab results from http call
+type result struct {
+	url    string
+	status string
+}
+
 var DestinationUrl *endpoints
 
 // getting urls stored in a yaml file
-func getURLs() *endpoints {
+func getURLs() []string {
 	viper.AddConfigPath(".")
 	viper.SetConfigName("urls")
 	viper.SetConfigType("yaml")
@@ -30,18 +37,49 @@ func getURLs() *endpoints {
 		log.Fatal(err)
 	}
 
-	return DestinationUrl
+	var urls []string
+	for _, url := range DestinationUrl.Urls {
+		urls = append(urls, url.Url)
+	}
+
+	return urls
+}
+
+// checking urls status
+func checkStatus(urls []string) {
+	// creating a buffered channel
+	resultChan := make(chan result, len(urls))
+
+	// ranging over urls, making GET requests and storing data in channel
+	for _, url := range urls {
+		go func(u string) {
+			resp, err := http.Get(u)
+			if err != nil {
+				log.Printf("couldnt make request for %s", u)
+			}
+			defer resp.Body.Close()
+
+			resultChan <- result{url: u, status: resp.Status}
+		}(url)
+	}
+
+	// looping over channel and printing result, I used select to trigger
+	// second case when timeout is reached
+	for i := 0; i < len(urls); i++ {
+		select {
+		case r := <-resultChan:
+			fmt.Printf("%s \t==>\t %s\n", r.url, r.status)
+		case <-time.After(5 * time.Second):
+			r := <-resultChan
+			fmt.Printf("%s \t==>\t %s\n", r.url, "request timeout (time > 5s)")
+		}
+	}
+	close(resultChan)
 }
 
 func main() {
 	// getting the urls
 	dest := getURLs()
-	fmt.Println(dest)
-	resp, err := http.Get("https://google.com")
-	if err != nil {
-		log.Println(err)
-	}
-	defer resp.Body.Close()
 
-	fmt.Println(resp.Status)
+	checkStatus(dest)
 }
